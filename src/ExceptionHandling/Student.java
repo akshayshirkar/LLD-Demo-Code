0d19e5e2fe304dc5b67c6c76b19d9a2eb6e6ef15
Akshay Shikar <akshayshirkardev@gmail.com>
2025-02-23 12:53:56 +0530

    LLD

---
+++ src/ExceptionHandling/Student.java
@@ -0,0 +1,7 @@
+package ExceptionHandling;
+
+public class Student {
+    public static int Calculation(int x){
+        return 1 / x;
+    }
+}
---
+++ src/ExceptionHandling/client.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+public class client {
+    public static void main(String[] args) {
+        System.out.println("Exception handling");
+        Student.Calculation(0);
+
+    }
+}
